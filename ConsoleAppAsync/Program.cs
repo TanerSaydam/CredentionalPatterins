e617879ae8a374bedd4e96e833caf25c5c23f114
taner <taner@DESKTOP-3BJ5GK9>
2022-11-01 04:42:59 +0300

    .

---
+++ ConsoleAppAsync/Program.cs
@@ -0,0 +1,50 @@
+﻿
+//Example ex = Example.GetInstance();
+var t1 = Task.Run(() =>
+{
+    Example.GetInstance();
+});
+
+var t2 = Task.Run(() =>
+{
+    Example.GetInstance();
+});
+
+//await Task.WhenAll(t1, t2);
+
+var t3 = Task.Run(() =>
+{
+    Example.GetInstance();
+});
+
+var t4 = Task.Run(() =>
+{
+    Example.GetInstance();
+});
+
+await Task.WhenAll(t1, t2,t3, t4);
+
+class Example
+{
+    static Example()
+    {
+        _example = new Example();
+    }
+    //static object _obj = new object();
+    public static Example GetInstance()
+    {
+        //lock (_obj)
+        //{
+        //    if (_example == null)
+        //        _example = new Example();
+        //}
+       
+        return _example;
+    }
+    private Example()
+    {
+        Console.WriteLine($"{nameof(Example)} is created.");
+    }
+
+    static Example _example;
+}
