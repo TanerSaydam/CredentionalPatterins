995cb1ac539312f03034890f7082a5a222d743ae
taner <taner@DESKTOP-3BJ5GK9>
2022-11-06 19:59:18 +0300

    .

---
+++ AbstractFactoryConsoleApp/Program.cs
@@ -0,0 +1,25 @@
+﻿Computer computer = new();
+CPU cpu = new CPU();
+computer.CPU = cpu;
+
+class Computer
+{
+    public CPU CPU { get; set; }
+    public RAM RAM { get; set; }
+    public VideoCard VideoCard { get; set; }
+}
+
+class CPU
+{
+
+}
+
+class RAM
+{
+
+}
+
+class VideoCard
+{
+
+}
